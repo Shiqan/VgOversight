16be8cf5d600337597e13162114a506ba864a9f3
Ferron <ferron@Robins-iMac.local>
2017-04-28 15:21:26 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import 
+import os
 
 appname = "Vainglorious Meta"
 app = Flask(__name__)
